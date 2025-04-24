e1b1a6825be4d2828120e1aea14f82a33fea7aaa
HwPhuc <lphuc507@gmail.com>
2025-04-24 14:16:32 +0700

    init project FE

---
+++ apartmentmanagementmobileapp/components/Home/Home.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import MyStyles from "../../styles/MyStyles";
+
+const Home = () => {
+    return (
+        <View style={MyStyles.container}>
+            <Text style={MyStyles.subject}>APARTMENT MANAGEMENT</Text>
+        </View>
+    );
+};
+
+export default Home;
---
+++ apartmentmanagementmobileapp/styles/MyStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    subject: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "blue",
+        marginTop: 50,
+    },
+});
